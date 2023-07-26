fa7c37df9fd76b72a2f8ecfc6ac7867e8224b061
quangtuan9997 <mrt.tuan97@gmail.com>
2023-07-26 15:19:48 +0700

    implement-department

---
+++ src/main/java/com/example/trianing_project/controller/CertificateController.java
@@ -0,0 +1,29 @@
+package com.example.trianing_project.controller;
+
+import com.example.trianing_project.service.dto.CertificateDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/employee/certificate")
+public class CertificateController {
+    @GetMapping("/add")
+    public ModelAndView showAdd(){
+        ModelAndView modelAndView = new ModelAndView("certificate/add");
+        modelAndView.addObject("certificate",new CertificateDTO());
+        return modelAndView;
+    }
+//    @PostMapping("/add")
+//    public ModelAndView doAdd(HttpServletRequest request, @ModelAttribute("certificate")CertificateDTO certificateDTO){
+//        HttpSession session=request.getSession();
+//        String email = (String) session.getAttribute("user");
+//
+//    }
+}
