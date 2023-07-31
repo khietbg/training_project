376b1f7fc2f1a1a16890527826b574768b870a72
minh khiet <minhkhiet@MacBook-Pro-cua-minh-2.local>
2023-07-31 13:48:43 +0700

    feat: implement login logout

--- src/main/java/com/example/trianing_project/controller/AuthController.java
+++ src/main/java/com/example/trianing_project/controller/AuthController.java
@@ -1,10 +1,12 @@
-package com.example.trianing_project.controller;
-
-import com.example.trianing_project.domain.Employee;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class AuthController {
-}
+//package com.example.trianing_project.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class AuthController {
+//    @GetMapping("/login")
+//    public String login(){
+//        return "login";
+//    }
+//}
